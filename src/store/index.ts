83648f69e15f0607f9f91572bc520ba81a8c0513
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-08-28 07:56:18 -0300

    Debugando com DevTools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from './modules/rootReducer'
 import { ICartState } from './modules/cart/types'
@@ -7,6 +8,6 @@ export interface IState {
   cart: ICartState
 }
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools())
 
 export default store
